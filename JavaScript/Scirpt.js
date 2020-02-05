355163c11f542c4c1f567afd8516e75d7fdde95c
axecution <alkhatibsaed007@gmail.com>
2020-02-05 12:47:35 +0200

    Added functions!

--- JavaScript/Scirpt.js
+++ JavaScript/Scirpt.js
@@ -4,18 +4,34 @@ var month = Number(dob.substr(4, 2)) - 1;
 var day = Number(dob.substr(6, 2));
 var today = new Date();
 var age = today.getFullYear() - year;
-if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
-  age--;
-}
+
+  function tellEm (){
 alert("your "+age+"s old");
 
 if(age>=25){
     alert("Daaym your old");
 }
 else if(age<24 && age>18 ) {
-    alert("Your too young to enjoy sleep keep, going!");
+    alert("Your too young to enjoy sleep , keep going!");
 }
 else 
 {
     alert("Your too young to be on this site go to sleep kid!");
 }
+  }
+
+tellEm();
+
+
+
+
+
+
+
+// var d = parseInt(dob);
+// function calcAge(d) {
+//     var birthday = +new Date(d);
+//     return ~~((Date.now() - birthday) / (31557600000));
+//   }
+
+// alert("your age is "+calcAge(d));
